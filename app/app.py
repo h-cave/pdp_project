9450e6efd70e57718c11409e0c958656359851d6
h-cave <124801533+h-cave@users.noreply.github.com>
2024-11-22 14:16:26 +0000

    Add an endpoint to kill the app for testing

--- app/app.py
+++ app/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+import json, os, signal
+
+@app.route('/stopServer', methods=['GET'])
+def stopServer():
+    os.kill(os.getpid(), signal.SIGINT)
+    return jsonify({ "success": True, "message": "Server is shutting down..." })
